042facb7eb352202a5a456562afa7c7d408f93a1
Mari-Anne Eerikäinen <mari-anne.eerikainen@awacademy.training>
2022-09-22 11:03:10 +0300

    staff menu repair

--- wwwroot/js/Layout.js
+++ wwwroot/js/Layout.js
@@ -1,16 +1,10 @@
 ﻿let stBtn = document.getElementById("staffBtn")
-let admBtn = document.getElementById("adminBtn")
-
 let stMenu = document.getElementById("staffActions")
-let admMenu = document.getElementById("adminActions")
 
 stBtn.addEventListener("click", () => {
     stMenu.classList.toggle("show")
 })
 
-admBtn.addEventListener('click', () => {
-    admMenu.classList.toggle('show')
-})
 
 window.onclick = function (e) {
     if (!e.target.matches("#staffBtn")) {
@@ -18,9 +12,5 @@ window.onclick = function (e) {
             stMenu.classList.remove("show");
         }
     }
-    if (!e.target.matches("#adminBtn")) {
-        if (admMenu.classList.contains("show")) {
-            admMenu.classList.remove("show");
-        }
-    }
+  
 }
