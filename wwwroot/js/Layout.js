ab400a7aa5bcf3be400a5bfa4f557c2933ca1cae
Mari-Anne Eerikäinen <mari-anne.eerikainen@awacademy.training>
2022-09-27 08:23:15 +0300

    minor error fixing

--- wwwroot/js/Layout.js
+++ wwwroot/js/Layout.js
@@ -16,21 +16,24 @@ window.onclick = function (e) {
         }
     }
 
-    if (!e.target.matches("#campTrigger")) {
-        if (campArea.classList.contains("show")) {
-            campArea.classList.remove("show");
-        }
-    }
+    //if (!e.target.matches("#campTrigger")) {
+    //    if (campArea.classList.contains("show")) {
+    //        campArea.classList.remove("show");
+    //        campArea.classList.add("hide");
+    //    }
+    //}
 
-    if (!e.target.matches("#camperTrigger")) {
-        if (camperArea.classList.contains("show")) {
-            camperArea.classList.remove("show");
-        }
-    }
+    //if (!e.target.matches("#camperTrigger")) {
+    //    if (camperArea.classList.contains("show")) {
+    //        camperArea.classList.remove("show");
+    //        camperArea.classList.add("hide");
+    //    }
+    //}
 
-    if (!e.target.matches("#staffTrigger")) {
-        if (counselorArea.classList.contains("show")) {
-            counselorArea.classList.remove("show");
-        }
-    }
+    //if (!e.target.matches("#staffTrigger")) {
+    //    if (counselorArea.classList.contains("show")) {
+    //        counselorArea.classList.remove("show");
+    //        counselorArea.classList.add("hide");
+    //    }
+    //}
 }
